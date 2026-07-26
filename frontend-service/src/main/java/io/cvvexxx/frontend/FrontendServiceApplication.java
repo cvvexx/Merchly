df2bb50581920c41d4c097ce2e4d6ca06cb3e450
Artem Potapov <araotuu376@gmail.com>
2026-07-26 19:13:55 +0300

    feature: added caching httpSession to redis

--- frontend-service/src/main/java/io/cvvexxx/frontend/FrontendServiceApplication.java
+++ frontend-service/src/main/java/io/cvvexxx/frontend/FrontendServiceApplication.java
@@ -3,10 +3,12 @@ package io.cvvexxx.frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication(exclude = {
         UserDetailsServiceAutoConfiguration.class
 })
+@EnableRedisHttpSession
 public class FrontendServiceApplication {
 
     public static void main(String[] args) {
--- frontend-service/src/main/java/io/cvvexxx/frontend/security/KeycloakJwtAuthenticationToken.java
+++ frontend-service/src/main/java/io/cvvexxx/frontend/security/KeycloakJwtAuthenticationToken.java
@@ -4,10 +4,13 @@ import lombok.Getter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 @Getter
-public class KeycloakJwtAuthenticationToken extends AbstractAuthenticationToken {
+public class KeycloakJwtAuthenticationToken extends AbstractAuthenticationToken implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private final String principal;
     private final String accessToken;
